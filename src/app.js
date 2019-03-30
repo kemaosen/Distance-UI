cd108246a87aac4edf95bb35075d4a95adc99b35
kms <kms_123@163.com>
2019-03-30 21:47:16 +0800

    添加单元测试

--- src/app.js
+++ src/app.js
@@ -16,3 +16,90 @@ Vue.component('g-button-group',ButtonGroup);
         }
     }
 })
+import chai from 'chai'
+const expect = chai.expect;
+// 单元测试
+{
+    const Constructor = Vue.extend(Button);
+    const vm  = new Constructor({
+        propsData:{
+            icon:'settings',
+        }
+    });
+    vm.$mount('#test');
+    let userElement = vm.$el.querySelector('use');
+    // 期待 userElement.getAttribute('xlink:href') === '#i-settings'
+    expect(userElement.getAttribute('xlink:href')).to.eq('#i-settings');
+    vm.$el.remove();
+    vm.$destroy();
+    // console.log(Constructor )
+}
+
+{
+    const Constructor = Vue.extend(Button);
+    const vm  = new Constructor({
+        propsData:{
+            icon:'settings',
+            loading:true,
+        }
+    });
+    vm.$mount();
+    let userElement = vm.$el.querySelector('use');
+    // 期待 userElement.getAttribute('xlink:href') === '#i-settings'
+    expect(userElement.getAttribute('xlink:href')).to.eq('#i-loading');
+    vm.$el.remove();
+    vm.$destroy();
+
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm  = new Constructor({
+        propsData:{
+            icon:'settings',
+            loading:true,
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq("1");
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const button  = new Constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'
+        }
+    });
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq("2");
+    button.$el.remove();
+    button.$destroy();
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm  = new Constructor({
+        propsData:{
+            icon:'settings',
+        }
+    });
+    vm.$mount();
+    gBtton.$on('click',function (){
+        console.log(1)
+    })
+    let button = vm.$el;
+    button.click();
+    button.$el.remove();
+    button.$destroy();
+}
