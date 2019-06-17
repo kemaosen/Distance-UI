b32b0ae49c7ca470a6dbb9a262610439bb599f59
kms <kms_123@163.com>
2019-06-17 09:14:23 +0800

    添加input change事件测试用例

--- test/input.test.js
+++ test/input.test.js
@@ -5,50 +5,51 @@ import Input from '../src/input'
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
+
+
 describe('Input', () => {
     it('存在.', () => {
         expect(Input).to.be.ok
     })
 
     describe('props', () => { 
+        const Constructor = Vue.extend(Input);
+        let vm
+
+        afterEach( () => { 
+            vm.$destroy()
+        })
+
         it('接收 value', () => { 
-            const Constructor = Vue.extend(Input);
-            const vm = new Constructor({
+            vm = new Constructor({
                 propsData: {
                     value:'1234'
                 }
             }).$mount()
             const inputElement = vm.$el.querySelector('input')
             expect(inputElement.value).to.equal('1234')
-            vm.$destroy()
         })
         it('接收 disabled', () => { 
-            const Constructor = Vue.extend(Input)
-            const vm = new Constructor({
+            vm = new Constructor({
                 propsData: {
                     disabled: true
                 }
             }).$mount()
             const inputElement = vm.$el.querySelector('input')
             expect(inputElement.disabled).to.equal(true)
-            vm.$destroy()
         })
 
         it('接收 readonly', () => { 
-            const Constructor = Vue.extend(Input)
-            const vm = new Constructor({
+            vm = new Constructor({
                 propsData: {
                     readonly: true
                 }
             }).$mount()
             const inputElement = vm.$el.querySelector('input')
-            console.log(inputElement.outerHTML);
             expect(inputElement.readOnly).to.equal(true)
-            vm.$destroy()
         })
         it('接收 error', () => {
-            const Consturctor = Vue.extend(Input)
-            const vm = new Consturctor({
+            vm = new Constructor({
                 propsData: {
                     error:'你好世界'
                 }
@@ -60,10 +61,23 @@ describe('Input', () => {
             // 获取input 组件中 errorMassage 元素(span)
             const errorMassage = vm.$el.querySelector('.errorMassage')
             expect(errorMassage.innerText).to.equal('你好世界')
-            vm.$destroy()
         })
     })
     describe('事件', () => { 
-        it('支持')
+        const Constructor = Vue.extend(Input);
+        let vm
+
+        afterEach( () => { 
+            vm.$destroy()
+        })
+        it('支持 change', () => { 
+            vm = new Constructor({}).$mount()
+            const callback = sinon.fake()
+            vm.$on('change', callback)
+            var event = new Event('change')
+            let inputElement = vm.$el.querySelector('input')
+            inputElement.dispatchEvent(event)
+            expect(callback).to.have.been.called
+        })
     })
 })
