63476d07a289ffe6d0bac47333a514882cfbb750
kms <kms_123@163.com>
2019-06-19 13:17:21 +0800

    添加row 和col 的雏形

---
+++ src/col.vue
@@ -0,0 +1,50 @@
+<!-- 页面 -->
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        span:{
+            type:[Number,String]
+        }
+    },
+    mounted () {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    watch: {},
+    filters: {},
+    computed: {},
+    components: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.col{
+    height: 40px;
+    background-color: gray;
+    width: 50%;
+    border: 1px solid red;
+    $class-prefix: col-;
+    @for $n from 1 through 24{
+        &.#{$class-prefix}#{$n}{
+            width: ( $n / 24 ) * 100%;
+        }
+    }
+}
+// scss插值 
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,34 @@
+<!-- 页面 -->
+<template>
+    <div class="row">
+        <slot ></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted () {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    watch: {},
+    filters: {},
+    computed: {},
+    components: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.row{
+    display: flex;
+}
+</style>
